3e7581ee6386bcc32e7bf35024e8f0bce101e64e
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-06-18 06:57:59 +0700

    Update extends, plugins, and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,15 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    "@nuxtjs",
+    "plugin:nuxt/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/vue"
   ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {}
-}
+  plugins: ["prettier"],
+  rules: { "vue/v-bind-style": "off" }
+};
